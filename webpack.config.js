0d5293912a2c7bc84da8398b932124054a1d08ed
arvis777 <naujausias001@gmail.com>
2020-09-22 09:49:06 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+let path = require('path');
+
+module.exports = {
+    entry: __dirname + "/src/app.js",
+    output: {
+        path: __dirname + "/public/",
+        filename: 'app.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/public'),
+        compress: true,
+        port: 9000
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
